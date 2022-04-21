9b3ebd2afd51a6bccf9c9ac1aa9a4457af6ee9b3
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-04-21 12:09:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import { nanoid } from "nanoid";
 
 function App () {
     const [questions, setQuestions] = useState([])
-    const [quiz, setQuiz] = useState(false)
-    
+    const [count, setCount] = useState(0)
+    const [game, setGame] = useState(false)
+    const [isChecked, setChecked] = useState(false)
+
     useEffect(()=> {
         async function getQuestions () {
             const res = await fetch ("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
@@ -26,59 +28,81 @@ function App () {
                 })
                 return {
                     question: he.decode(item.question),
-                    answers: answerArr.sort((a, b) => Math.random() - 0.5), //Shuffles the answer
-                    correctAnswer: he.decode(item.correct_answer),
+                    answers: answerArr.sort((a, b) => Math.random() - 0.5), //Shuffles the answer,
                     isAnswered: false,
+                    correctAnswer: he.decode(item.correct_answer),
                     id: nanoid()
                 }
             }))
         }
         getQuestions()
-    },[])
-
+     }, [])
+    
     function selectAnswer(event) {
         const clickedAnswer = (event.target.innerHTML)
         const clickedQuestion = (event.target.parentElement.parentNode.firstChild.innerHTML)
-            const updatedQuiz = questions.map(function(question) {
-                if(question.question === clickedQuestion) {
-                    const updatedAnswers = question.answers.map(function(answer) {
-                        return answer.answer === clickedAnswer ? 
-                        {...answer, isSelected: true, isAnswered: true} : 
-                        {...answer, isSelected: false, isAnswered: false}             
-                    })
-                    return {...question, answers:updatedAnswers, isAnswered: true}
-                } else {
-                    return {...question, isAnswered: false}
-                } 
-        })
-        setQuestions(prevQuiz => updatedQuiz)
-    }
 
-    
-    useEffect(()=> {
-        const allQuestions = questions.every(item => item.isAnswered)
-        console.log("All:" + allQuestions)
-        // if(allQuestions) {
-        //     console.log("True")
-        // }
-    }, [questions]) 
-   
+        setQuestions((prevQuiz) => 
+        validateQuiz(prevQuiz, clickedAnswer, clickedQuestion))
+    }
+    console.log(questions)
+
+    const validateQuiz = (questions, clickedAnswer, clickedQuestion) => 
+        questions.map((question) => {
+        if(question.question === clickedQuestion) {
+            const updatedAnswers = question.answers.map(function(answer) {
+                return answer.answer === clickedAnswer ? 
+                {...answer, isSelected: true, isAnswered: true} : 
+                {...answer, isSelected: false, isAnswered: false}             
+                })
+                  return {...question, answers:updatedAnswers, 
+                    isAnswered: updatedAnswers.some(ans=> ans.isSelected)}
+               } else {
+                  return question
+              } 
+        })
 
     function checkAnswers () {
-        return quiz ? console.log("Answers checked") : console.log("You have not answered all questions")
+        const allQuestions = questions.every(question => question.isAnswered)
+        if(allQuestions) {
+            const correctAnswers = questions.reduce((count, question) => {
+                const index = question.answers.findIndex(answer => answer.isSelected)
+                const Correct = question.answers[index].isCorrectAnswer
+                if(Correct) {
+                   return count + 1
+                }
+                return count
+            })
+            setCount(correctAnswers)
+            console.log(`You got ${count}/5 questions right`)
+        } else {
+            return "Make sure to answer all the questions"
+        }
     }
+
+    function toggleCheck () {
+        setChecked(prev => !prev)
+    }
+
+    function handleClick () {
+        if(questions.every(question => question.isAnswered)) toggleCheck()
+    }
+        
 
     return (
         <main>
-            <div className="circle--top"></div>
+        <div className="circle--top"></div>
             <Quiz questions={questions}
             selectAnswer={selectAnswer}
              />
-             <button 
-                className="check--button"
-                onClick={checkAnswers}
-                >Check answers</button>
-             <div className="circle--bottom"></div>
+             {/* {isChecked ? checkAnswers() : "Make sure to answer all the questions"} */}
+           <div className="answer--container">
+                <button 
+                        className="check--button"
+                        onClick={checkAnswers}
+                        >Check answers</button>
+           </div>
+        <div className="circle--bottom"></div>
         </main>
     )
 }
